f2633b437b4b97347a6ded684046835690c6baa6
3f27 <3f27@Liceospalla.local>
2016-12-02 10:07:19 +0100

    first commit

---
+++ TestClassi/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace TestClassi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(166, 127);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(126, 24);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(203, 196);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(488, 183);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(962, 483);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.radioButton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ TestClassi/Monster.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TestClassi
+{
+    class Monster
+    {
+        public string name;
+        public int maxHp;
+        public int curHp;
+        public int damage;
+        
+        public Monster(string name, int maxHp, int damage)
+        {
+            this.name = name;
+            this.maxHp = maxHp;
+            curHp = maxHp;
+            this.damage = damage;
+            describe();
+        }
+
+        public string describe()
+        {
+            string output = "Questo è " + name + "\r\n";
+            output += "HP: " + curHp + "\r\n";
+            output += "DAMAGE: " + damage + "\r\n";
+
+            return output;
+        }
+
+        public void describe(TextBox t)
+        {
+            t.Text = describe();
+        }
+
+        public void attack(Monster target)
+        {
+            if (target.curHp <= 0)
+            {
+                Console.WriteLine(target.name + " è già esausto, non infierire.");
+                return;
+            }
+
+            Console.WriteLine(name + " attacca " + target.name);
+            Console.WriteLine(name + " fa " + damage + " danni a " + target.name);
+            target.curHp -= damage;
+            
+            if (target.curHp <= 0 )
+            {
+                target.curHp = 0;
+                Console.WriteLine(target.name + " è esausto.");
+            } else
+            {
+                Console.WriteLine("a " + target.name + " rimangono " + target.curHp + " hp");
+            }
+        }
+    }
+}
